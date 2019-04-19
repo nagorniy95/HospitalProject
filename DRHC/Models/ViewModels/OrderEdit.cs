46ba0b00dd10589d787480e3471ff377cbf8ddfc
rohit <rohit@DESKTOP-RJRPDOO>
2019-04-18 21:24:59 -0700

    order controller updated

---
+++ DRHC/Models/ViewModels/OrderEdit.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DRHC.Models.ViewModels
+{
+    public class OrderEdit
+    {
+        public OrderEdit()
+        {
+
+        }
+
+        
+        public virtual Order Order { get; set; }
+
+        public IEnumerable<Guest> Guests { get; set; }
+
+        public IEnumerable<Menu> Menus { get; set; }
+    }
+}
